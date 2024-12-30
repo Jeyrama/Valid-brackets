f16b6a32eea703ed0f91a75be6fea60c5b70fab4
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-12-29 21:20:15 -1000

    Update Valid-brackets.js

--- Valid-brackets.js
+++ Valid-brackets.js
@@ -19,3 +19,23 @@ Examples:
 
 
 // Solution
+
+function validBraces(braces){
+  let matches = { '(':')', '{':'}', '[':']' };
+  let stack = [];
+  let currentChar;
+
+  for (let i=0; i<braces.length; i++) {
+    currentChar = braces[i];
+
+    if (matches[currentChar]) { // opening braces
+      stack.push(currentChar);
+    } else { // closing braces
+      if (currentChar !== matches[stack.pop()]) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0; // any unclosed braces left?
+}
