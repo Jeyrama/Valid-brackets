2aedc24d3b2eff4637dcf9d25249a4eec021a573
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2024-12-29 21:20:53 -1000

    Update Valid-brackets.js

--- Valid-brackets.js
+++ Valid-brackets.js
@@ -41,3 +41,15 @@ function validBraces(braces){
 }
 
 // or
+
+function validBraces(braces){
+  for (let i = 0, depth = []; i < braces.length; i++) {
+    switch (braces[i]) {
+      case '(': case '[': case '{': depth.push(braces[i]); break;
+      case ')': if (depth.pop() != '(') return false; break;
+      case ']': if (depth.pop() != '[') return false; break;
+      case '}': if (depth.pop() != '{') return false; break;
+    }
+  }
+  return depth.length == 0;
+}
